4b3e98849f219b0abf707aa590e93762b7d2349a
Todd Whitelow <todd@whitelow.tech>
2021-01-17 21:30:48 -0700

    added add review button to prof show page

--- src/views/ProfessorShow.vue
+++ src/views/ProfessorShow.vue
@@ -5,7 +5,7 @@
     <p>School: {{ professor[0].school }}</p>
     <p>Title: {{ professor[0].title }}</p>
     <p>Department: {{ professor[0].department }}</p>
-
+    <button> Add Review </button>
 
     <h2>Reviews</h2>
     <div v-for="review in professor[0].reviews">
