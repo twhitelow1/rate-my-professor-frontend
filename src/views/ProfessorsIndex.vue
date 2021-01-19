9589c2e8e471fa97de033f812e36bc0ab1a5fb2b
seldenleonard <selden222@gmail.com>
2021-01-18 20:49:53 -0500

    adds functionality to search bar

--- src/views/ProfessorsIndex.vue
+++ src/views/ProfessorsIndex.vue
@@ -1,19 +1,54 @@
 <template>
   <div class="professors">
     <div align="center">
-      <input type="text" name="search" /> <button>Search</button>
-      <h1>
-        --------------------------------------------------------------------------------------------------
-      </h1>
-      <div v-for="professor in professors">
-        <h2>{{ professor.name }} - {{ professor.school }}</h2>
-        <router-link v-bind:to="`/professors/${professor.id}`" tag="button"
-          >View Professor</router-link
+      <!-- PORTFOLIO FILTER -->
+      <datalist id="titles">
+        <option v-for="professor in professors">{{ professor.name }}</option>
+      </datalist>
+      <ul id="filters" class="filter font-inc hidden-xs p-t-20">
+        <li>
+          <div class="form-group">
+            <strong
+              ><input
+                class="form-control input-md"
+                v-model="allAttributeFilter"
+                placeholder="Search"
+                style="margin-right: 10px"
+            /></strong>
+            <button>Search</button>
+            <h1>
+              --------------------------------------------------------------------------------------------------
+            </h1>
+          </div>
+        </li>
+      </ul>
+      <!-- /PORTFOLIO FILTER -->
+
+      <!-- PORTFOLIO ITEM -->
+      <ul>
+        <li
+          v-for="professor in orderBy(
+            filterBy(
+              professors,
+              allAttributeFilter,
+              'name',
+              'title',
+              'department',
+              'school'
+            ),
+            sortAttribute
+          )"
         >
-        <h1>
-          --------------------------------------------------------------------------------------------------
-        </h1>
-      </div>
+          <h2>{{ professor.name }} - {{ professor.school }}</h2>
+          <router-link v-bind:to="`/professors/${professor.id}`" tag="button"
+            >View Professor</router-link
+          >
+          <h1>
+            --------------------------------------------------------------------------------------------------
+          </h1>
+        </li>
+      </ul>
+      <!-- /PORTFOLIO ITEM -->
     </div>
   </div>
 </template>
@@ -22,21 +57,29 @@
 
 <script>
 import axios from "axios";
+import Vue2Filters from "vue2-filters";
+
 export default {
-  data: function() {
+  mixins: [Vue2Filters.mixin],
+  data: function () {
     return {
       professors: [],
+      allAttributeFilter: "",
+      sortAttribute: "",
     };
   },
-  created: function() {
+  created: function () {
     this.indexProfessors();
   },
   methods: {
-    indexProfessors: function() {
+    indexProfessors: function () {
       axios.get("api/professors").then((response) => {
         console.log("professors index", response);
         this.professors = response.data;
       });
+    },
+    setSortAttribute: function (attribute) {
+      this.sortAttribute = attribute;
     },
   },
 };
